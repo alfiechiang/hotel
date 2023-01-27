177e3fac7d8ddb4d066162d0cc59bf41356c7672
zack <zack@zackdeMacBook-Pro.local>
2023-01-27 23:04:24 +0800

    update

--- src/views/room_type/index.vue
+++ src/views/room_type/index.vue
@@ -3,20 +3,83 @@
 
 
     <div class="box">
-        <el-button color="#626aef">新增房型</el-button>
+        <el-button color="#626aef" @click="createDialogVisible = true">新增房型</el-button>
 
         <div class="content">
-            <el-table  ref="dataTableRef" :data="List" style="width:100%">
+            <el-table ref="dataTableRef" :data="List" style="width:100%">
+                <el-table-column prop="typeID" label="房型編號" width="120" />
+
                 <el-table-column prop="name" label="房間名稱" width="120" />
                 <el-table-column prop="price" label="價格" width="120" />
                 <el-table-column prop="bed_num" label="床位數" width="600" />
                 <el-table-column fixed="right" label="Operations" width="120">
-                    <template #default>
-                        <el-button link type="primary" size="small">編輯</el-button>
+                    <template #default="scope">
+                        <el-button link type="primary" size="small" @click="onEdit(scope.row)">編輯</el-button>
                     </template>
                 </el-table-column>
             </el-table>
         </div>
+
+        <div class="pager">
+            <el-pagination current-page="4" page-size="10" :page-sizes="[10, 20, 30, 40]"
+                layout="total, sizes, prev, pager, next, jumper" @current-change="handleCurrentChange" :total="total" />
+        </div>
+
+        <el-dialog v-model="createDialogVisible" title="新增房型" width="50%">
+            <el-form :model="form" label-width="68px">
+                <el-form-item label="房型編號">
+                    <el-input v-model.number="form.typeID" />
+                </el-form-item>
+                <el-form-item label="房型名稱">
+                    <el-input v-model="form.name" />
+                </el-form-item>
+                <el-form-item label="價錢">
+                    <el-input v-model.number="form.price" />
+                </el-form-item>
+                <el-form-item label="床位數">
+                    <el-select v-model.number="form.bed_num" placeholder="請選擇床位數">
+                        <el-option label="1" value=1 />
+                        <el-option label="2" value="2" />
+                        <el-option label="3" value="3" />
+                        <el-option label="4" value="4" />
+                        <el-option label="5" value="5" />
+                        <el-option label="6" value="6" />
+                    </el-select> </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">新增</el-button>
+                    <el-button @click="onCancel">取消</el-button>
+                </el-form-item>
+            </el-form>
+        </el-dialog>
+
+
+
+        <el-dialog v-model="editDialogVisible" title="編輯房型" width="50%">
+            <el-form :model="form" label-width="68px">
+                <el-form-item label="房型編號">
+                    <el-input v-model.number="form.typeID" />
+                </el-form-item>
+                <el-form-item label="房型名稱">
+                    <el-input v-model="form.name" />
+                </el-form-item>
+                <el-form-item label="價錢">
+                    <el-input v-model.number="form.price" />
+                </el-form-item>
+                <el-form-item label="床位數">
+                    <el-select v-model.number="form.bed_num" placeholder="請選擇床位數">
+                        <el-option label="1" value=1 />
+                        <el-option label="2" value="2" />
+                        <el-option label="3" value="3" />
+                        <el-option label="4" value="4" />
+                        <el-option label="5" value="5" />
+                        <el-option label="6" value="6" />
+                    </el-select> </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onUpdate">更新</el-button>
+                    <el-button @click="onCancel">取消</el-button>
+                </el-form-item>
+            </el-form>
+        </el-dialog>
 
 
 
@@ -28,70 +91,136 @@
 <script setup lang="ts">
 
 import request from '@/utils/request';
-import {reactive,toRefs,onBeforeMount} from 'vue';
+import { reactive, toRefs, onBeforeMount, ref } from 'vue';
+import { ElMessage } from 'element-plus'
+
+export interface RommTypeSearch {
+    page: number | undefined;
+    page_size: number | undefined;
+}
+
+export interface RommTypeForm {
+    name: string | undefined;
+    price: number;
+    typeID: number
+    bed_num: number;
+}
+
+const state = reactive({
+    List: [],
+    search: {} as RommTypeSearch,
+    form: {} as RommTypeForm
+})
+const {
+    List,
+    search,
+    form
+} = toRefs(state);
 
 
-export interface RoomTypeList  {
-    ID :number
-    roomID: number
-    name:string
-    price:number
-    bed_num:number
+
+let currentPage = ref(1)
+let total = ref(0)
+let pageSize = ref(10)
+const handleCurrentChange = (val: number) => {
+    currentPage.value = val
+    roomTypeList()
+}
+
+const onCancel = () => {
+    createDialogVisible.value = false
+    editDialogVisible.value =false
+    form.value = {} as RommTypeForm
+}
+
+const onSubmit = async () => {
+
+    await request({
+        url: '/admin/roomtype',
+        method: "post",
+        data: form.value
+    }).then((res: any) => {
+        if (res.code == 200) {
+            ElMessage({ message: '新增成功', type: 'success' })
+        }
+
+    })
+    roomTypeList()
+    form.value = {} as RommTypeForm
+    createDialogVisible.value = false
+
+}
+
+const onEdit = async (row: any) => {
+    await request({
+        url: '/admin/' + row.typeID + '/roomtype',
+        method: 'get',
+    }).then((res:any) => {
+        form.value =res.data
+        
+    })
+    editDialogVisible.value =true
+}
+
+
+const onUpdate = async() =>{
+    await request({
+        url: '/admin/roomtype/'+form.value.typeID,
+        method: "put",
+        data: form.value
+    }).then((res: any) => {
+        if (res.code == 200) {
+            ElMessage({ message: '更新成功', type: 'success' })
+
+        }
+
+        roomTypeList()
+        editDialogVisible.value =false
+        form.value = {} as RommTypeForm
+    })
 }
 
 
 
-const state = reactive({
-    List :[]
-
-})
-const {
-    List
-} = toRefs(state);
 
 
-const tableData = [
-  {
-    roomID: 1,
-    name: 'Tom',
-    price: 123,
-    bed_num:4
-  },
-]
+const roomTypeList = async () => {
 
+    search.value.page = currentPage.value;
+    search.value.page_size = pageSize.value;
+    await request({
+        url: '/admin/roomtype',
+        method: 'get',
+        params: search.value
+    }).then((res) => {
 
+        List.value = res.data.data
+        total.value = res.data.total
 
-const roomTypeList = async()=>{
-   await request({
-      url: '/admin/roomtype',
-      method: 'get',
-   }).then((res) => {
-
-    List.value =res.data
-
-   })
+    })
 }
 
 onBeforeMount(() => {
     roomTypeList();
 })
 
-
-
-
-
-
-
-
+const createDialogVisible = ref(false)
+const editDialogVisible =ref(false)
 
 </script>
 <style lang="scss">
-
 .box {
     width: 100%;
-    height: 500px;
+    height: 600px;
     padding: 20px;
     background: white;
 }
 
+.pager {
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 </style>
